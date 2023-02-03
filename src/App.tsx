6b6af6b316613efca226f644ac53b3ae9caade98
geiger01 <tantangeiger@gmail.com>
2023-02-03 16:33:03 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <form action="">
+        <label htmlFor="">Enter phone numbers:</label>
+        <textarea name="" id="" cols={30} rows={10}></textarea>
+        <label htmlFor="">Enter whatsapp message:</label>
+        <textarea name="" id="" cols={30} rows={10}></textarea>
+        <button>Send</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
